e087f6b29482c6bbf1207a6cf6076411b0bc054d
“osamede20” <osamede.asemota@gsscogs.uk>
2023-06-01 12:36:17 +0100

    renamed some columns

--- state-of-the-water-environment/wrangling.py
+++ state-of-the-water-environment/wrangling.py
@@ -11,8 +11,10 @@ def wrangle(input: Path(), output: Path()) -> None:
     df1 = pd.read_csv("b3a.csv")
     df2 = pd.read_csv("b3b.csv")
     df3 = pd.read_csv("b3c.csv")
-
     df = pd.concat([df1, df2, df3], axis=0, ignore_index=True, sort=False)
+    
+    df.to_csv('b3.csv', index=False)
+    df = pd.read_csv("b3.csv")
 
     df["Year"] = df["variable"].str.extract(r"(\d+)") 
     df["variable"] = df["variable"].str.extract(r"([^(\d+)]+)")
@@ -32,7 +34,7 @@ def wrangle(input: Path(), output: Path()) -> None:
     )
 
     df["Region"] = "E92000001"
-    df["Measure"] = df.apply(
+    df["Measure Type"] = df.apply(
     lambda x: "status-of-surface-waters"
     if x["variable"] == "" and x["site"] == ""
     else "status-of-ground-waters"
@@ -51,7 +53,7 @@ def wrangle(input: Path(), output: Path()) -> None:
 
     df["site"] = df["site"].str.extract(r"([^(\d+)]+)")
     df = df.fillna("")
-    df.rename(columns={"Year": "Period", "status": "Status", "percentage": "Value"}, inplace=True)
+    df.rename(columns={"status": "Status", "percentage": "Observation"}, inplace=True)
     df["Environment Surveyed"] = df.apply(
     lambda x: x["water_body"]
     if x["variable"] == "" and x["site"] == ""
@@ -65,7 +67,7 @@ def wrangle(input: Path(), output: Path()) -> None:
 
     df.drop(columns=['water_body', 'variable', 'site'], axis = 1, inplace=True)
     df.replace({'Environment Surveyed': {'Rivers: Plants & algae': 'Rivers: Plants and algae'}}, inplace=True)
-    df = df[['Period', 'Region', 'Environment Surveyed', 'Status', 'Measure Type', 'Unit', 'Value']]
+    df = df[['Year', 'Region', 'Environment Surveyed', 'Status', 'Measure Type', 'Unit', 'Observation']]
 
     df.to_csv(output, index=False)
     return
