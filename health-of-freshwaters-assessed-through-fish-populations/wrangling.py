2bea66fc099b8592ff14cb16cb0e533d74d78157
bailed22 <95625808+bailed22@users.noreply.github.com>
2023-07-25 16:37:25 +0100

    health-of-freshwaters-assessed-through-fish-populations (#5)

---
+++ health-of-freshwaters-assessed-through-fish-populations/wrangling.py
@@ -0,0 +1,21 @@
+from email.policy import default
+import click
+import pandas as pd
+from pathlib import Path
+
+@click.command()
+@click.argument("input", type=click.Path(exists=True, path_type=Path))
+@click.option("--output", default=Path("./output.csv"), type=click.Path(path_type=Path))
+def wrangle(input: Path(), output: Path()) -> None:
+
+    df = pd.read_csv(input, na_values="na")
+    
+    df.rename(columns={'year': 'Year','status': 'Status of salmon river','percentage': 'Observation'})
+
+    df['Observation'] = df['Observation'].astype(float).round(2)
+
+    df.to_csv(output, index=False)
+    return
+
+if __name__ == "__main__":
+    wrangle()
